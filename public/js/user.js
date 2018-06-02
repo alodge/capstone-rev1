f9df80302b6e0970a9a9dcc8c573fc013b753aa0
alodge <lodgea@oregonstate.edu>
2018-06-02 15:41:37 -0700

    Update user.js

--- public/js/user.js
+++ public/js/user.js
@@ -1,176 +1,27 @@
+const Eth = require('ethjs-query')
+const EthContract = require('ethjs-contract')
 
-// First
-// var abi = JSON.parse(
-//  '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
-//);
+window.addEventListener('load', function() {
 
-// second
-// var abi = JSON.parse(
-//   '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]'
-// );
+  // Check if Web3 has been injected by the browser:
+  if (typeof web3 !== 'undefined') {
 
-// third
-var abi = JSON.parse(
-	'[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_extraData","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'  
-);
+    // You have a web3 browser! Continue below!
+    startApp(web3);
 
-// putting my test into it
-var web3;
-var eth;
-var BurgerContract;
-
-$(document).ready(function() {
-  // Checking if Web3 has been injected by MetaMask
-  if (typeof web3 !== "undefined" && web3.currentProvider.isMetaMask) {
-    web3 = new Web3(web3.currentProvider);
-    eth = web3.eth;
   } else {
-    $("#balance_text").hide();
-    $("#meta_text").show();
+	  // Warn the user that they need to get a web3 browser
+	  // Or install MetaMask, maybe with a nice graphic.
+	  console.log("get metamask");
   }
-  // initial
-  //var BurgerContract = eth
-  //  .contract(abi)
-  //  .at("0xa1f2112d28d9c159940928dc5d7ff45f869a2350");
-	
-  // second
-  // BurgerContract = eth
-  // .contract(abi)
-  // .at("0xe8f31079989eca482d84a95c9ff145da8db3e612");
 
-  // third
-  BurgerContract = eth
-  	.contract(abi)
-  	.at("0x5c87e1c456dcedfab139df81a7ca331bb0c14dda");
-	
-  var text = $("#balance_text > strong").text();
-  console.log("in user");
-  console.log(text);
-  
-  // Get user's balance
-  BurgerContract.balanceOf.call(eth.coinbase, function(error, result) {
-    if (error) {
-      console.log(error);
-    } else {
-      var bal = result / 1000000000000000000;
-      var bal = result;
-      console.log(bal);
-      $("#balance_text > strong").text(text + bal);
-    }
-  });
-  
-});
+})
 
-// beginning of function call
-/*
-// Execute Transfer
-async function transferCoin(){
-  console.log("in transfer js function");
-  
-  // var t_web3 = new Web3(web3.currentProvider);
-  // var t_eth = t_web3.eth;
-  var t_contract = eth
-    .contract(abi)
-    .at("0xe8f31079989eca482d84a95c9ff145da8db3e612");
-  
-  // var transferee = u_transfer_to.value;
-  // var transfer_amount = u_transfer_amount.value;
-  // web3.t_eth.defaultAccount = web3.eth.accounts[0];
-  
-  console.log(web3.eth.accounts[0]);
-  console.log(u_transfer_to.value);
-  console.log(u_transfer_amount.value);
-  
-  var fromAddress = web3.eth.accounts[0];
-  var toAddress = u_transfer_to.value;
-  var sendAmount = u_transfer_amount.value;
-  
-  console.log("just before calling transfer contract");
- 
-  var tranString = {}
-  tranString.gas = 70000;
-  tranString.from = fromAddress;
-  var tranObject = JSON.stringify(tranString);
-  console.log(tranString);
-  console.log(tranObject);
-  
-  // also callback 
-  // t_contract.transfer.call(toAddress, sendAmount, { from: fromAddress });
-  
-  
-  // This one is also mad about syncronicity
-  BurgerContract.symbol.call(function(error, result) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("in not-error");
-      var bal = result;
-      console.log(bal);
-      return bal;
-    }
-  });
-  
-  // console.log(contractResponse);
-  console.log("at end of js function");
-  
-  /*
-  // This one is unhappy because "The MetaMask Web3 object does not support synchronous methods"
-  var contractResponse = await t_contract.transfer(toAddress, sendAmount).send({ gas: "700000", from: fromAddress });
-  console.log(contractResponse);
-  */
-// }
-// */
-// end of function call
+function startApp(web3) {
 
+  const eth = new Eth(web3.currentProvider)
+  const contract = new EthContract(eth)
 
-document.addEventListener('DOMContentLoaded', reqButton);
+  initContract(contract)
 
-function reqButton()
-{
-	document.getElementById("transferButton").addEventListener('click', function(event)
-	{
-		var t_contract = eth
-    			.contract(abi)
-    			.at("0xe8f31079989eca482d84a95c9ff145da8db3e612");
-		var fromAddress = web3.eth.accounts[0];
-  		var toAddress = u_transfer_to.value;
-  		var sendAmount = u_transfer_amount.value;
-		transferCoin(fromAddress, toAddress, sendAmount);
-		
-		/*
-		BurgerContract.transfer.call(toAddress, sendAmount, function(error, result) {
-    			if (error) {
-      				console.log(error);
-    			} else {
-      				console.log("in not-error");
-			      	var bal = result;
-      				console.log(bal);
-    			}
-		});
-		*/
-		// call
-		event.preventDefault();
-	})
-}
-
-async function transferCoin(from, to, qty) {
-  try {
-    console.log("transferCoin");
-    var moveAttempt = await moveCoin(from, to, qty);
-  } catch(error) {
-    console.log(error);
-  }
-}
-
-function moveCoin(fromAddress, toAddress, qty) {
-  return new Promise(function(resolve, reject) {
-    BurgerContract.transfer.call(toAddress, qty, function(error, response) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(response);
-	      console.log(response);
-      }
-    })
-  });
 }

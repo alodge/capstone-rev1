1e35518f2c1efe5df60498fc70f8f76a8ad2b128
alodge <lodgea@oregonstate.edu>
2018-06-03 10:54:07 -0700

    Update bundle.js

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -10441,7 +10441,8 @@ function initContract (contract, eth) {
     		} else {
       			// var bal = result / 1000000000000000000;
       			// result => result.toNumber();
-			var balString = result[0];
+			var bal = result[0];
+			var balString = bal.toString();
       			console.log(balString);
 			// var balNumber = result.toNumber();
 			// console.log(balNumber);
